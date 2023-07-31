96a0c44cccc3596cd352a365616e483d2456b5d4
JohnLee0115 <johnlee011500@gmail.com>
2023-07-30 18:38:05 -0700

    first commit

---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("My name is John Lee");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Glendale, CA");
+    }
+}
